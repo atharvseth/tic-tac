2b14ae15741b054404f9826355c3b353ebe1accf
atharvseth <Atharvabhiseth@gmail.com>
2026-01-07 19:55:53 +0530

    tic tac toe

---
+++ activity1.py
@@ -0,0 +1,41 @@
+"we will make  the board using dictionary in which keys will be the location(ie: top left,mid-right.etc)and initially it's value will be empty and then after every mave we will change value according to player choice of move."
+the_board = {'7':' ','8':' ','9':' ',
+             '4':' ','5':' ','6':' ',
+             '1':' ','2':' ','3':' '}
+
+board_keys = []
+
+for key in the_board:
+    board_keys.append(key)
+
+def printboard(board):
+    print(board['7'] + '|' + board['8'] + '|'  + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+def game():
+
+    turn = 'X'
+    count = 0
+
+    for i in range(10):
+        printboard(the_board)
+        print("it's your turn," + turn + "move to which place?" )
+
+        move = input()
+
+        if the_board[move] == '':
+            the_board[move] = turn
+            count += 1
+
+        else:
+            print("that place is already filled. \nmove to which place? ")
+
+            continue
+        if count >= 5:
+            if the_board['7'] == the_board['8'] == the_board['9'] != ' ':
+                printboard(the_board)
+                print("\ngame over. \n")
+                
